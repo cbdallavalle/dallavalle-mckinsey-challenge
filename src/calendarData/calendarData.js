fc0d51e91f3945331ad2b01f7c22ce43fd45cdd4
Casey Dallavalle <cbdallavalle@gmail.com>
2018-03-25 10:09:35 -0600

    Add days of month functionality Upon componentDidMount the current year is found and also the number of days in the year. The days in the current month are an array that contains objects with the day (a number to represent the weekdays) and the date

--- src/calendarData/calendarData.js
+++ src/calendarData/calendarData.js
@@ -1,3 +1,12 @@
 export const calendarData = {
-  months: ["Januray", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  months: ["Januray", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+  weekday: ["Sunday", "Monay", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+  mockEvents: [
+    {
+      start: '8:30am',
+      end: '9:30am'
+    }
+  ]
 }
+
+// Each object contains a start time and an end time for the event.
--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -6,25 +6,54 @@ class Calendar extends Component {
   constructor() {
     super();
     this.state = {
-      currMonth: ''
+      currYear: 2017,
+      currMonth: 0,
+      currDaysInMonth: [],
     }
   }
 
-  componentDidMount = () => {
-    this.determineCurrentMonth();
+  componentDidMount = async () => {
+    await this.determineCurrentMonth();
+    await this.determineCurrentYear();
+    await this.getDaysInMonth(this.state.currMonth, this.state.currYear);
+  }
+
+  determineCurrentYear = () => {
+    const date = new Date();
+    const currYear = date.getFullYear()
+    this.setState({ currYear })
   }
 
   determineCurrentMonth = () => {
     const date = new Date();
-    const currMonth = calendarData.months[date.getMonth()];
+    const currMonth = date.getMonth()
     this.setState({ currMonth });
   }
 
   changeMonth = ({ target }) => {
     const direction = target.id;
+    let currMonth = this.state.currMonth;
+    direction === "forward" ? currMonth += 1 : currMonth -= 1;
+    this.setState({ currMonth });
   }
 
+  getDaysInMonth(month, year) {
+     const date = new Date(year, month, 1);
+     let currDaysInMonth = [];
+     while (date.getMonth() === month) {
+        const dateAllInfo = new Date(date);
+        const dateInfo = {
+          day: dateAllInfo.getDay(),
+          date: dateAllInfo.getDate()
+        }
+        currDaysInMonth.push(dateInfo);
+        date.setDate(date.getDate() + 1);
+     }
+     this.setState({ currDaysInMonth });
+  }
+  
   render() {
+    console.log(this.state)
     return (
       <article className="Calendar">
         <h1>Casey's Calendar</h1>
@@ -34,7 +63,7 @@ class Calendar extends Component {
             id="backward" 
             onClick={ this.changeMonth }>
           </i>
-          <h2> { this.state.currMonth } </h2>
+          <h2> { calendarData.months[this.state.currMonth] } </h2>
           <i 
             className="fas fa-chevron-right" 
             id="forward" 
